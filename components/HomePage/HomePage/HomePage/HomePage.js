de3165f383fb579aa41aea3d94596f690b9c96f4
reactwarriors5 <reactwarriors@official@gmail.com>
2021-09-21 20:14:44 +0600

    latest

---
+++ components/HomePage/HomePage/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+const HomePage = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+};
+
+export default HomePage;
---
+++ components/HomePage/HomePage/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <section>
+      <h1>This is navbar</h1>
+    </section>
+  );
+};
+
+export default Navbar;
