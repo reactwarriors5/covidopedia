83df8686c5897fe674f62acd5a1c1c6ac7864292
aa-nadim <nadim.ice.nstu@gmail.com>
2021-09-22 11:08:26 +0600

    Footer complete

--- components/HomePage/HomePage/HomePage/HomePage.js
+++ components/HomePage/HomePage/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
 const HomePage = () => {
   return (
     <main>
       <Navbar />
+      <Footer />
     </main>
   );
 };
