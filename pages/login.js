72414176c4e3d8f9be68cf3f6ba2991b4aaea9e1
reactwarriors5 <reactwarriors@official@gmail.com>
2021-09-27 11:07:58 +0600

    login

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../components/Auth/SignIn/SignIn";
+
+const login = () => {
+  return (
+    <>
+      <SignIn />
+    </>
+  );
+};
+
+export default login;
