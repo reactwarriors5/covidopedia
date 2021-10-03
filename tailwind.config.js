1b69a84488046e470be921de2a75042304650b12
Habiba Akhter <habibaakhter2012@gmail.com>
2021-10-03 08:58:23 +0200

    Revert "Habiba"

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,7 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      backgroundImage: (theme) => ({
-        Emergency: "url('/images/images-adnan/emergency-CTA.jpg')",
-        BookAppointment: "url('/images/images-adnan/appointment.jpg')",
-        Testimonial: "url('/images/images-adnan/testimonial.jpg')",
-      }),
-      zIndex: {
-        "-10": "-10",
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
