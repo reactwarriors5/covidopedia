8e59e7cd17b4ca5e974bebbc74d0dc9c180bdfe0
habiba akhter <habibaakhter2012@gmail.com>
2021-10-03 09:47:35 +0200

    habiba new

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import HomePage from "../components/HomePage/HomePage/HomePage/HomePage";
+import "antd/dist/antd.css";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-     <HomePage/>
-    
-    </div>
-  )
-
+    <>
+      <Head>
+        <title>Covidopedia</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HomePage />
+    </>
+  );
 }
