5afe5a5af3ecff23f2a95bc3b557ba12796a6192
Asadullah Al Noman <noman.ice.nstu@gmail.com>
2021-10-20 15:04:48 +0600

    optimize code

--- components/DepartmentPages/TeamMember.js
+++ components/DepartmentPages/TeamMember.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import doctorData from './FakeData/doctorData.json';
 import TeamMemberDetails from "./TeamMemberDetails";
 import axios from 'axios'
 
