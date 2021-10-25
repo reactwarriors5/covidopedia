a10c60fc8428d4e1dd65458ea750bcceebe85e09
shazim8480 <troublemaker8480@gmail.com>
2021-10-25 09:17:47 +0600

    changes

---
+++ pages/covidPortalDetails.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CovidPortalDetails from '../components/HomePage/HomePage/CovidPortalDetails/CovidPortalDetails'
+
+const covidPortalDetails = () => {
+    return (
+        <>
+         <CovidPortalDetails/>   
+        </>
+    )
+}
+
+export default covidPortalDetails
