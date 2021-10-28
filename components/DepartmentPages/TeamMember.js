c549492d34e93f94abf3eca5aad3f61af7ca03d5
reactwarriors5 <reactwarriors@official@gmail.com>
2021-10-28 20:16:49 +0600

    done

--- components/DepartmentPages/TeamMember.js
+++ components/DepartmentPages/TeamMember.js
@@ -1,36 +1,45 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios'
-import TeamMemberDetails from './TeamMemberDetails'
-axios.defaults.withCredentials = true
+import axios from "axios";
+import TeamMemberDetails from "./TeamMemberDetails";
+axios.defaults.withCredentials = true;
 
 const TeamMember = () => {
-  const [doctors, setDoctors] = useState([])
+  const [doctors, setDoctors] = useState([]);
 
   useEffect(() => {
     const getDoctors = async () => {
-      const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/doctors`)
-      setDoctors(data)
-      console.log(data)
-    }
-    getDoctors()
-  }, [])
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API}/doctors`
+      );
+      setDoctors(data);
+      console.log(data);
+    };
+    getDoctors();
+  }, []);
   return (
     <>
       {doctors && (
-        <div className='mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
-          <h1 className='text-3xl font-bold '>Team Member</h1>
-          <div className='grid gap-12 mx-auto lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-3'>
-            {doctors.map(doctor => (
-              <TeamMemberDetails
-                key={doctor._id}
-                doctor={doctor}
-              ></TeamMemberDetails>
-            ))}
+        <section className="py-6 text-gray-800 bg-gray-100">
+          <div className="container flex flex-col items-center justify-center p-4 mx-auto sm:p-10">
+            <p className="p-2 text-sm font-semibold tracking-wider text-center text-indigo-700 uppercase">
+              COVID Response Team
+            </p>
+            <h4 className="text-4xl font-bold leading-none text-center sm:text-5xl">
+              The Dedicated People Behind The Scenes
+            </h4>
+            <div className="flex flex-row flex-wrap-reverse justify-center mt-8">
+              {doctors.map((doctor) => (
+                <TeamMemberDetails
+                  key={doctor._id}
+                  doctor={doctor}
+                ></TeamMemberDetails>
+              ))}
+            </div>
           </div>
-        </div>
+        </section>
       )}
     </>
-  )
-}
+  );
+};
 
-export default TeamMember
+export default TeamMember;
