f00943b89ec87e195f64d17338c6f48f6613eaca
Asadullah Al Noman <noman.ice.nstu@gmail.com>
2021-10-28 14:49:48 +0600

    finally complete super admin dashboard

---
+++ pages/allDoctors.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Doctors from "../components/Admin/SuperAdmin/Doctors/Doctors";
+
+const allDoctors = () => {
+  return (
+    <>
+      <Doctors />
+    </>
+  );
+};
+
+export default allDoctors;
