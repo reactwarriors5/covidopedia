894a5cfd152e33e70fc04623b01cd52719f1a5aa
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2021-10-31 15:45:41 +0600

    modify vendor pages

--- components/Pharmacy/PharmaSidebar/PharmaSidebar.js
+++ components/Pharmacy/PharmaSidebar/PharmaSidebar.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import { } from '@fortawesome/free-brands-svg-icons'
 
 const PharmaSidebar = () => {
     return (
@@ -16,13 +19,31 @@ const PharmaSidebar = () => {
                         <input type="text" class="w-full h-12 border border-gray-800 rounded px-3" placeholder="Select Location" />
 
                         <div>
-                            <h3>Categories</h3>
-                            <p>Popular</p>
-                            <p>Latest</p>
-                            <p>Ratings</p>
-                            <p>Availability</p>
-                            <p>Open 24 Hrs</p>
-                            <p>Home Delivery</p>
+                            <h3 className="">Categories</h3>
+                            <div className="flex justify-start items-center ">
+                                <FontAwesomeIcon className="" icon={faCheckSquare} />
+                                <p className="ml-2">Popular</p>
+                            </div>
+                            <div className="flex justify-start items-center ">
+                                <FontAwesomeIcon className="" icon={faSquare} />
+                                <p className="ml-2">Latest</p>
+                            </div>
+                            <div className="flex justify-start items-center ">
+                                <FontAwesomeIcon className="" icon={faSquare} />
+                                <p className="ml-2">Ratings</p>
+                            </div>
+                            <div className="flex justify-start items-center ">
+                                <FontAwesomeIcon className="" icon={faSquare} />
+                                <p className="ml-2">Availability</p>
+                            </div>
+                            <div className="flex justify-start items-center ">
+                                <FontAwesomeIcon className="" icon={faSquare} />
+                                <p className="ml-2">Open 24 Hrs</p>
+                            </div>
+                            <div className="flex justify-start items-center ">
+                                <FontAwesomeIcon className="" icon={faSquare} />
+                                <p className="ml-2">Home Delivery</p>
+                            </div>
                         </div>
 
 
