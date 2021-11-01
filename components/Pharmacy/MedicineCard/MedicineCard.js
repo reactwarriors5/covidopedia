0a0544aeba088e4d1dc6661910c5d2d48699d58a
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2021-11-01 20:21:17 +0600

    resolved

--- components/Pharmacy/MedicineCard/MedicineCard.js
+++ components/Pharmacy/MedicineCard/MedicineCard.js
@@ -10,7 +10,7 @@ const MedicineCard = () => {
             <div  class="p-8 bg-white rounded shadow-md flex card text-grey-darkest">
                 <img class="w-1/3  rounded-l-sm" src={medicine.Image} alt="Room Image" />
                 <div class="ml-10">
-                    <h1 className="text-lg font-bold ">{medicine.name}</h1>
+                    <h1 className="text-lg font-bold">{medicine.name}</h1>
                     <p>Manufactured By Hamdard (Wakf) Laboratories</p>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
