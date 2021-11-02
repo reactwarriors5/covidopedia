331b588a5ff31d9f8f08b65642f0457ed9664830
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2021-11-02 21:07:23 +0600

    modify cart

--- pages/addToCart.js
+++ pages/addToCart.js
@@ -41,37 +41,11 @@ const addToCart = () => {
                             <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
                         </div>
 
-                        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-                            <div class="flex w-2/5">
-                                <div class="w-20">
-                                    <img class="h-24" src="https://drive.google.com/uc?id=10ht6a9IR3K2i1j0rHofp9-Oubl1Chraw" alt=""/>
-                                </div>
-                                <div class="flex flex-col justify-between ml-4 flex-grow">
-                                    <span class="font-bold text-sm">Xiaomi Mi 20000mAh</span>
-                                    <span class="text-red-500 text-xs">Xiaomi</span>
-                                    <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
-                                </div>
-                            </div>
-                            <div class="flex justify-center w-1/5">
-                                <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                                </svg>
-
-                                <input class="mx-2 border text-center w-8" type="text" value="1"/>
-
-                                <svg class ="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-                                <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-                                </svg>
-                            </div>
-                            <span class="text-center w-1/5 font-semibold text-sm">$40.00</span>
-                            <span class="text-center w-1/5 font-semibold text-sm">$40.00</span>
-                        </div>
-
-                       
                         <a href="#" class="flex font-semibold text-indigo-600 text-sm mt-10">
-
                             <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
                             Continue Shopping
                         </a>
+
                     </div>
                     <div id="summary" class="w-1/4 px-8 py-10">
                         <h1 class="font-semibold text-2xl border-b pb-8">Order Summary</h1>
