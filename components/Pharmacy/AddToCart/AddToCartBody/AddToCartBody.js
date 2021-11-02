2f308b009718e70103a2d9ba4d62f277d1f59a64
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2021-11-02 21:28:27 +0600

    modify addtocart body part

--- components/Pharmacy/AddToCart/AddToCartBody/AddToCartBody.js
+++ components/Pharmacy/AddToCart/AddToCartBody/AddToCartBody.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Link from 'next/link'
 
 const AddToCartBody = () => {
     return (
         <div class="w-3/4 bg-white px-10 py-10">
             <div class="flex justify-between border-b pb-8">
                 <h1 class="font-semibold text-2xl">Shopping Cart</h1>
-                <h2 class="font-semibold text-2xl">3 Items</h2>
+                <h2 class="font-semibold text-2xl">1 Items</h2>
             </div>
             <div class="flex mt-10 mb-5">
                 <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
@@ -37,11 +38,12 @@ const AddToCartBody = () => {
                 <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
                 <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
             </div>
-
-            <a href="#" class="flex font-semibold text-indigo-600 text-sm mt-10">
-                <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
-                Continue Shopping
-            </a>
+            <Link href="/pharmacyMedicines">
+                <a class="flex font-semibold text-indigo-600 text-sm mt-10">
+                    <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
+                    Continue Shopping
+                </a>
+            </Link>
         </div>
     );
 };
