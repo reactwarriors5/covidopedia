15fc0f0333f71a83873e727119440950f7174bc4
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2021-11-02 21:16:13 +0600

    add nevbar in addtocart

--- pages/addToCart.js
+++ pages/addToCart.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import Navbar from '../components/HomePage/HomePage/Navbar/Navbar';
 import AddToCartBody from '../components/Pharmacy/AddToCart/AddToCartBody/AddToCartBody';
 import AddToCartSidebar from '../components/Pharmacy/AddToCart/AddToCartSidebar/AddToCartSidebar';
 
 const addToCart = () => {
     return (
-        <div class="bg-gray-100">
-            <div class="container mx-auto mt-10">
-                <div class="flex shadow-md my-10">
-                    <AddToCartBody />
-                    <AddToCartSidebar />
+        <>
+            <Navbar />
+            <div class="">
+                <div class="container mx-auto mt-10">
+                    <div class="flex shadow-md my-10">
+                        <AddToCartBody />
+                        <AddToCartSidebar />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
