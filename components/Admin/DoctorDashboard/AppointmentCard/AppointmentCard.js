0d780d1740292946dab2216ac5c808d11d7a1205
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-11-03 22:45:57 +0600

    pdf creation done

--- components/Admin/DoctorDashboard/AppointmentCard/AppointmentCard.js
+++ components/Admin/DoctorDashboard/AppointmentCard/AppointmentCard.js
@@ -1,49 +1,50 @@
-import React from "react";
-import PatientModal from "../PatientModal/PatientModal"
+import React from 'react'
+import PatientModal from '../PatientModal/PatientModal'
+import PrescriptionModal from '../PatientModal/PrescriptionModal'
 
 const AppointmentCard = ({ appointmentCardData }) => {
   // console.log(appointmentCardData)
   return (
-    <div className="items-center px-8 py-3 lg:px-8">
-      <div className="p-6 mx-auto bg-white border rounded-lg shadow-md lg:w-full">
+    <div className='items-center px-8 py-3 lg:px-8'>
+      <div className='p-6 mx-auto bg-white border rounded-lg shadow-md lg:w-full'>
         {/* details div -- left */}
-        <div className="flex flex-col items-center justify-between py-2 rounded-lg lg:flex-row">
-          <div className="flex items-start justify-start w-full lg:justify-start lg:w-1/2">
+        <div className='flex flex-col items-center justify-between py-2 rounded-lg lg:flex-row'>
+          <div className='flex items-start justify-start w-full lg:justify-start lg:w-1/2'>
             {/* image here */}
             <img
-              src="https://storage.googleapis.com/indie-hackers.appspot.com/avatars/300x300_DUFTlhOdmSdoT9F4r1Woo40pyML2.webp"
-              alt="placeholder"
-              className="rounded-lg"
+              src='https://storage.googleapis.com/indie-hackers.appspot.com/avatars/300x300_DUFTlhOdmSdoT9F4r1Woo40pyML2.webp'
+              alt='placeholder'
+              className='rounded-lg'
             />
             {/* patient details here */}
-            <div className="flex flex-col w-full text-gray-500 lg:ml-4">
-              <h4 className="my-4 text-lg font-bold tracking-widest text-indigo-600 uppercase lg:mt-0 title-font">
+            <div className='flex flex-col w-full text-gray-500 lg:ml-4'>
+              <h4 className='my-4 text-lg font-bold tracking-widest text-indigo-600 uppercase lg:mt-0 title-font'>
                 {appointmentCardData.patient.name}
               </h4>
-              <p className="mb-3 text-base leading-relaxed text-gray-600">
+              <p className='mb-3 text-base leading-relaxed text-gray-600'>
                 {appointmentCardData.createdAt}
               </p>
-              <p className="mb-3 text-base leading-relaxed text-gray-600">
+              <p className='mb-3 text-base leading-relaxed text-gray-600'>
                 {/* {appointmentCardData.patientAddress} */}
                 Dhaka
               </p>
-              <p className="mb-3 text-base leading-relaxed text-gray-600">
+              <p className='mb-3 text-base leading-relaxed text-gray-600'>
                 {/* {appointmentCardData.patientEmail} */}
               </p>
-              <p className="mb-3 text-base leading-relaxed text-gray-600">
+              <p className='mb-3 text-base leading-relaxed text-gray-600'>
                 {/* {appointmentCardData.patientPhone} */}
               </p>
             </div>
           </div>
           {/* actions & buttons div -- right */}
-          <div className="flex items-center justify-start">
+          <div className='flex items-center space-x-6 justify-start'>
             <PatientModal appointmentCardData={appointmentCardData} />
-            <button className="ml-3 btn-home">Process</button>
+            <PrescriptionModal appointmentCardData={appointmentCardData} />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AppointmentCard;
+export default AppointmentCard
---
+++ components/Admin/DoctorDashboard/PatientModal/PrescriptionModal.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react'
+import 'antd/dist/antd.css'
+import { Modal } from 'antd'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { SyncOutlined } from '@ant-design/icons'
+axios.defaults.withCredentials = true
+
+const PrescriptionModal = ({ appointmentCardData }) => {
+  const [loading, setLoading] = useState(false)
+  const { name, age, gender, patient, doctor, message, _id } =
+    appointmentCardData
+  const [presData, setPresData] = useState({
+    patientName: name,
+    gender: gender,
+    age: age,
+    message: message,
+    patient: patient._id,
+    doctor: doctor,
+    medicine: '',
+    feedback: '',
+  })
+  const [patientEmail, setPatientEmail] = useState({
+    appointmentId: _id,
+  })
+
+  const handleChange = e => {
+    const newUserInfo = { ...presData }
+    newUserInfo[e.target.name] = e.target.value
+    setPresData(newUserInfo)
+  }
+
+  const createPdf = e => {
+    e.preventDefault()
+    setLoading(true)
+
+    axios
+      .post(`${process.env.NEXT_PUBLIC_API}/create-pdf`, presData)
+      .then(() => {
+        axios.post(`${process.env.NEXT_PUBLIC_API}/upload-pdf`, patientEmail)
+        toast.success('Prescription created & sent to patients email.')
+        setLoading(false)
+        setIsModalVisible(false)
+      })
+  }
+
+  console.log(appointmentCardData)
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleOk = () => {
+    setIsModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+
+  const router = useRouter()
+  //   const { id } = router.query
+  //   const [doctor, setDoctor] = useState({})
+  //   useEffect(() => {
+  //     const getDoctor = async () => {
+  //       const { data } = await axios.get(
+  //         `${process.env.NEXT_PUBLIC_API}/doctor/${id}`
+  //       )
+  //       setDoctor(data)
+  //     }
+  //     getDoctor()
+  //   }, [id])
+
+  return (
+    <>
+      <button className='btn-home' onClick={showModal}>
+        Process
+      </button>
+      <Modal
+        title='Appointment Details'
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <section className='max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800'>
+          <h2 className='text-lg font-semibold text-gray-700 capitalize dark:text-white'>
+            Generate Prescription
+          </h2>
+
+          <form onSubmit={createPdf}>
+            <div className='grid grid-cols-1 gap-6 mt-4 '>
+              <div>
+                <label
+                  className='text-gray-700 dark:text-gray-200'
+                  htmlFor='medicine'
+                >
+                  Medicine
+                </label>
+                <input
+                  id='medicine'
+                  name='medicine'
+                  onChange={handleChange}
+                  type='text'
+                  className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
+                />
+              </div>
+              <div>
+                <label
+                  className='text-gray-700 dark:text-gray-200'
+                  htmlFor='feedback'
+                >
+                  Feedback
+                </label>
+                <input
+                  id='feedback'
+                  onChange={handleChange}
+                  name='feedback'
+                  type='text'
+                  className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
+                />
+              </div>
+            </div>
+
+            <div className='flex justify-end mt-6'>
+              <button
+                type='submit'
+                disabled={loading}
+                className='px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700'
+              >
+                {loading ? <SyncOutlined spin /> : 'SUBMIT'}
+              </button>
+            </div>
+          </form>
+        </section>
+      </Modal>
+    </>
+  )
+}
+
+export default PrescriptionModal
