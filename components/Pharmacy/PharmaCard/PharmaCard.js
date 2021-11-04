e4be927ca69c1e44d101ad11e6b4dc8aa6f14745
Asadullah Al Noman <noman.ice.nstu@gmail.com>
2021-11-04 10:40:08 +0600

    update ui

--- components/Pharmacy/PharmaCard/PharmaCard.js
+++ components/Pharmacy/PharmaCard/PharmaCard.js
@@ -44,7 +44,7 @@ const PharmaCard = () => {
             {
                 pharmaShop.map((pharma) => <PharmaCardDetails pharma={pharma}></PharmaCardDetails>)
             }
-            <div className="btn">
+            <div className="btn mt-5">
                 <button class="bg-indigo-700 hover:bg-indigo-700  text-white  active:bg-indigo-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button"
                 >
                     Load More
