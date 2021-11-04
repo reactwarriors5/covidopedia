a70ef7804bc875aaa840a2163f029393d252f172
Asadullah Al Noman <noman.ice.nstu@gmail.com>
2021-11-04 14:19:45 +0600

    modify dahsboard heading

--- components/Admin/DashboardHeader/DashboardHeader.js
+++ components/Admin/DashboardHeader/DashboardHeader.js
@@ -49,7 +49,14 @@ const DashboardHeader = ({ sidebarOpen, setSidebarOpen }) => {
                 <div onClick={isOpen}>
                   <button type="button" class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                     <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                    <img class="h-8 w-8 rounded-full" 
+                     src={
+                      user !== null && user.user && user.user.role === 'doctor'
+                        ? user.user.image
+                        : 'https://source.unsplash.com/100x100/?portrait'
+                    }  
+                    alt=""
+                     />
                   </button>
                 </div>
 
